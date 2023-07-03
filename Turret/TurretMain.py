219478756f3ac18bf902906d73260d514617c8df
Whateveryeti26 <mees_pronk@hotmail.com>
2023-07-03 18:16:55 +0200

    ultrasoon sensor (no connector availeble)

--- Turret/TurretMain.py
+++ Turret/TurretMain.py
@@ -7,10 +7,13 @@ last_tipped_time = 0
 tipped_timer = 5
 while True: 
 	current_time = time.time()
-	x, y , z = AccelerometerReadings() # from TurretSensorReadings
+	x, y , z = AccelerometerReadings()
+	# ~ distance = UltrasonicDistanceSensor()
+	# ~ sense.show_message(format(distance))
 	if ((0<x<=0.9)and(0<y<0.3)and(-0.5<z<0.3)):
 		Turret_Pickup()
 	elif ((x<0.3)and(-0.5<y>0.5)or(-0.5<z>0.5)):
 		if current_time - last_tipped_time>=tipped_timer:
 			 Turret_Tipped()
 			 last_tipped_time = current_time
+
--- Turret/TurretSensorReadings.py
+++ Turret/TurretSensorReadings.py
@@ -1,5 +1,16 @@
 from sense_hat import SenseHat
+import time
+import RPi.GPIO as GPIO
+
 sense = SenseHat()
+GPIO.setmode(GPIO.BCM)
+
+trig_pin = 17
+echo_pin = 27
+
+GPIO.setup(trig_pin, GPIO.OUT)
+GPIO.setup(echo_pin, GPIO.IN)
+
 def AccelerometerReadings():
 		raw = sense.get_accelerometer_raw()
 		x = raw['x']
@@ -12,3 +23,18 @@ def AccelerometerReadings():
 		print(f"x={x}, y={y},z={z}")
 		return x, y , z
 		
+def UltrasonicDistanceSensor():
+	GPIO.output(trig_pin, GPIO.HIGH)
+	time.sleep(0.00001)
+	GPIO.output(trig_pin, GPIO.LOW)
+	
+	GPIO.wait_for_edge(echo_pin, GPIO.RISING)
+	pulse_start = time.time()
+	
+	GPIO.wait_for_edge(trig_pin, GPIO.FALLING)
+	pulse_end = time.time()
+		
+	pulse_duration = pulse_end - pulse_start
+	distance = pulse_duration * 17150  # Speed of sound = 343 m/s
+	distance = round(distance, 2)
+	return distance
